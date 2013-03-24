6e92fbb7751cdc466f828692ecafa3b8a8818b1b
Jeremy Herr <jeremy.herr@gmail.com>
2013-03-24 00:19:35 +0000

    Added calculateHashes()
     - mimics what in-page JavaScript does to create hashes

--- RouterControl.py
+++ RouterControl.py
@@ -1,5 +1,6 @@
 import settings
 from urllib2 import Request, urlopen, HTTPError, HTTPCookieProcessor, install_opener, build_opener
+import hashlib
 
 class RouterControl:
 	"""Class for logging into O2 Wireless router and changing settings"""
@@ -29,6 +30,13 @@ class RouterControl:
 		except HTTPError, e:
 			error_message = str(e.code) + e.msg
 
+	def calculateHashes(self, realm, nonce, qop, uri):
+		"""Calculate the MD5 checksums that the in-page JavaScript uses to send password over http"""
+		HA1 = hashlib.md5(self.username + ":" + realm + ":" + self.password).hexdigest()
+		HA2 = hashlib.md5("GET" + ":" + uri).hexdigest()
+		return hashlib.md5(HA1 + ":" + nonce + ":" + "00000001" + ":" + "xyz" + ":" + qop + ":" + HA2).hexdigest()
+
 if __name__ == "__main__":
 	rc = RouterControl()
 	print rc.getLoginPage()
+	print rc.calculateHashes("Technicolor Gateway", "1851954:341337:204731139790f1a60397421eea9eb90f", "auth", "/login.lp")
